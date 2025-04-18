0e6d31689fe237e15b207040d8fca91130e5a955
şeyma topal <seymatopal16@outlook.com>
2025-04-18 03:00:12 +0300

    şeyma <3

--- Seyehat_Planlama_V/Form1.cs
+++ Seyehat_Planlama_V/Form1.cs
@@ -16,5 +16,10 @@ namespace Seyehat_Planlama_V
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Seyma <3");
+        }
     }
 }
